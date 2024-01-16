3767fa04188b7817cd8cfda464ecb4e1828d38f7
nkechr1s <nkechris22@gmail.com>
2024-01-17 01:20:27 +0200

    Fixes

--- main.go
+++ main.go
@@ -6,10 +6,7 @@ import (
 )
 
 func main() {
-	if err := converter.ConvertJSONToCSV("translations.json", "exports/translations.csv"); err != nil {
-		log.Fatal(err)
-	}
-	if err := converter.ConvertCSVToJSON("exports/translations.csv", "exports/new_translations.json"); err != nil {
+	if err := converter.ConvertJSONToCSV("translations.json", "csv_to_json/exports/translations.csv"); err != nil {
 		log.Fatal(err)
 	}
 }
